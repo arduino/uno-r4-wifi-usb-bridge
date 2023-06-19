a7f4179cc7487f91e90ca9a1a78f0cee73b0ba0a
Martino Facchin <m.facchin@arduino.cc>
2023-06-19 09:42:02 +0200

    Rename sketch to UNOR4USBBridge

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 # modify esp32 platform.txt from g++11 to gcc+17
 
-arduino-cli compile --fqbn=esp32:esp32:esp32s3:JTAGAdapter=default,PSRAM=disabled,FlashMode=qio,FlashSize=4M,LoopCore=1,EventsCore=1,USBMode=default,CDCOnBoot=default,MSCOnBoot=default,DFUOnBoot=default,UploadMode=default,PartitionScheme=huge_app,CPUFreq=240,UploadSpeed=921600,DebugLevel=none,EraseFlash=none -v CompostaUSBBridge
+arduino-cli compile --fqbn=esp32:esp32:esp32s3:JTAGAdapter=default,PSRAM=disabled,FlashMode=qio,FlashSize=4M,LoopCore=1,EventsCore=1,USBMode=default,CDCOnBoot=default,MSCOnBoot=default,DFUOnBoot=default,UploadMode=default,PartitionScheme=huge_app,CPUFreq=240,UploadSpeed=921600,DebugLevel=none,EraseFlash=none -v UNOR4USBBridge
