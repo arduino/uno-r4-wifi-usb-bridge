611ebbfa16f25f62e433e6184ad1c0d75e8b4c10
pennam <m.pennasilico@arduino.cc>
2023-06-26 16:18:16 +0200

    download.sh: Rename sketch to UNOR4USBBridge

--- download.sh
+++ download.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-arduino-cli compile --fqbn=esp32:esp32:esp32s3:JTAGAdapter=default,PSRAM=disabled,FlashMode=qio,FlashSize=4M,LoopCore=1,EventsCore=1,USBMode=default,CDCOnBoot=default,MSCOnBoot=default,DFUOnBoot=default,UploadMode=default,PartitionScheme=huge_app,CPUFreq=240,UploadSpeed=921600,DebugLevel=none,EraseFlash=none CompostaUSBBridge -u -p  /dev/ttyACM0 -v
+arduino-cli compile --fqbn=esp32:esp32:esp32s3:JTAGAdapter=default,PSRAM=disabled,FlashMode=qio,FlashSize=4M,LoopCore=1,EventsCore=1,USBMode=default,CDCOnBoot=default,MSCOnBoot=default,DFUOnBoot=default,UploadMode=default,PartitionScheme=huge_app,CPUFreq=240,UploadSpeed=921600,DebugLevel=none,EraseFlash=none UNOR4USBBridge -u -p  /dev/ttyACM0 -v
 
