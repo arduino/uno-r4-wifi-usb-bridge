9924d766f1e9f545d510b7d0683ea51fd369796e
pennam <m.pennasilico@arduino.cc>
2023-07-05 00:15:05 +0200

    Add symbolic links to esptool and toolchain

--- compile.sh
+++ compile.sh
@@ -10,4 +10,7 @@ cd ../../../
 
 sed  s#PWD#$PWD#g arduino-cli.yaml.orig > arduino-cli.yaml
 
+ln -s $(pwd)/hardware/esp32-patched/esp32/esptool hardware/esp32-patched/esp32/tools/esptool
+ln -s $(pwd)/hardware/esp32-patched/esp32/xtensa-esp32s3-elf hardware/esp32-patched/esp32/tools/xtensa-esp32s3-elf
+
 arduino-cli compile --config-file arduino-cli.yaml --fqbn=esp32-patched:esp32:esp32s3:JTAGAdapter=default,PSRAM=disabled,FlashMode=qio,FlashSize=4M,LoopCore=1,EventsCore=1,USBMode=default,CDCOnBoot=default,MSCOnBoot=default,DFUOnBoot=default,UploadMode=default,PartitionScheme=huge_app,CPUFreq=240,UploadSpeed=921600,DebugLevel=none,EraseFlash=none -v UNOR4USBBridge
