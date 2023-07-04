b218c25e5ef64d8d66cdc3841194036679e36733
pennam <m.pennasilico@arduino.cc>
2023-07-05 00:14:34 +0200

    Fix fqbn

--- compile.sh
+++ compile.sh
@@ -10,4 +10,4 @@ cd ../../../
 
 sed  s#PWD#$PWD#g arduino-cli.yaml.orig > arduino-cli.yaml
 
-arduino-cli compile --config-file arduino-cli.yaml --fqbn=esp32-git:esp32:esp32s3:JTAGAdapter=default,PSRAM=disabled,FlashMode=qio,FlashSize=4M,LoopCore=1,EventsCore=1,USBMode=default,CDCOnBoot=default,MSCOnBoot=default,DFUOnBoot=default,UploadMode=default,PartitionScheme=huge_app,CPUFreq=240,UploadSpeed=921600,DebugLevel=none,EraseFlash=none -v UNOR4USBBridge
+arduino-cli compile --config-file arduino-cli.yaml --fqbn=esp32-patched:esp32:esp32s3:JTAGAdapter=default,PSRAM=disabled,FlashMode=qio,FlashSize=4M,LoopCore=1,EventsCore=1,USBMode=default,CDCOnBoot=default,MSCOnBoot=default,DFUOnBoot=default,UploadMode=default,PartitionScheme=huge_app,CPUFreq=240,UploadSpeed=921600,DebugLevel=none,EraseFlash=none -v UNOR4USBBridge
