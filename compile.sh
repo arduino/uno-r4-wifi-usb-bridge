a45dfe4956f38a0538f947b6dd76af5a07e4ce76
Martino Facchin <m.facchin@arduino.cc>
2023-07-04 15:11:13 +0200

    wip: add script to vendor esp32 core

--- compile.sh
+++ compile.sh
@@ -1,3 +1,13 @@
 # modify esp32 platform.txt from g++11 to gcc+17
 
-arduino-cli compile --fqbn=esp32:esp32:esp32s3:JTAGAdapter=default,PSRAM=disabled,FlashMode=qio,FlashSize=4M,LoopCore=1,EventsCore=1,USBMode=default,CDCOnBoot=default,MSCOnBoot=default,DFUOnBoot=default,UploadMode=default,PartitionScheme=huge_app,CPUFreq=240,UploadSpeed=921600,DebugLevel=none,EraseFlash=none -v UNOR4USBBridge
+mkdir -p hardware/esp32-patched
+cd hardware/esp32-patched
+git clone https://github.com/espressif/arduino-esp32 -b 2.0.9 --depth=1 esp32
+cd esp32
+patch -p1 < ../../../esp32-core.patch
+./tools/get.py
+cd ../../../
+
+sed  s#PWD#$PWD#g arduino-cli.yaml.orig > arduino-cli.yaml
+
+arduino-cli compile --config-file arduino-cli.yaml --fqbn=esp32-git:esp32:esp32s3:JTAGAdapter=default,PSRAM=disabled,FlashMode=qio,FlashSize=4M,LoopCore=1,EventsCore=1,USBMode=default,CDCOnBoot=default,MSCOnBoot=default,DFUOnBoot=default,UploadMode=default,PartitionScheme=huge_app,CPUFreq=240,UploadSpeed=921600,DebugLevel=none,EraseFlash=none -v UNOR4USBBridge
