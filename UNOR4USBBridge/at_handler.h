3f4e668d1318feeaa7088c788e7fd88a26a5a671
pennam <m.pennasilico@arduino.cc>
2023-08-16 10:57:39 +0200

    Publish 0.3.0

--- UNOR4USBBridge/at_handler.h
+++ UNOR4USBBridge/at_handler.h
@@ -14,10 +14,10 @@
 #define MAX_SERVER_AVAILABLE   4
 #define MAX_UDP_AVAILABLE      4
 
-#define ESP_FW_VERSION    "0.2.1"
+#define ESP_FW_VERSION    "0.3.0"
 #define FIRMWARE_MAYOR    0
-#define FIRMWARE_MINOR    2
-#define FIRMWARE_PATCH    1
+#define FIRMWARE_MINOR    3
+#define FIRMWARE_PATCH    0
 
 #define GPIO_BOOT   9
 #define GPIO_RST    4
