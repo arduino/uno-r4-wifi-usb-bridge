86800645c2d2abfb25d2e786ccc325c6b8165225
pennam <m.pennasilico@arduino.cc>
2024-02-12 10:01:42 +0100

    certificates: use Mozilla ca bundle
      - https://github.com/espressif/esp-idf/blob/master/components/mbedtls/esp_crt_bundle/cacrt_all.pem

--- export.sh
+++ export.sh
@@ -3,7 +3,7 @@ rm -rf UNOR4USBBridge/build
 echo "Build Sketch"
 ./compile.sh
 echo "Build certificates bundle"
-python certificates/gen_crt_bundle.py -i certificates/certificates.pem
+python certificates/gen_crt_bundle.py -i certificates/cacrt_all.pem
 mv x509_crt_bundle UNOR4USBBridge/build/esp32-patched.esp32.arduino_unor4wifi_usb_bridge/x509_crt_bundle
 echo "Combine binaries"
 python combine.py
