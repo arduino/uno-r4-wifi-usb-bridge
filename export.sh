4efc0667c2135f112bee66769161e580f284786b
Mattia Pennasilico <m.pennasilico@arduino.cc>
2024-02-13 11:42:25 +0100

    Revert "Root certificates: use Mozilla ca bundle"

--- export.sh
+++ export.sh
@@ -4,7 +4,7 @@ rm -f UNOR4USBBridge/build/esp32-patched.esp32.arduino_unor4wifi_usb_bridge/x509
 echo "Build Sketch"
 ./compile.sh
 echo "Build certificates bundle"
-python certificates/gen_crt_bundle.py -i certificates/cacrt_all.pem
+python certificates/gen_crt_bundle.py -i certificates/certificates.pem
 mv x509_crt_bundle UNOR4USBBridge/build/esp32-patched.esp32.arduino_unor4wifi_usb_bridge/x509_crt_bundle
 echo "Combine binaries"
 python combine.py
